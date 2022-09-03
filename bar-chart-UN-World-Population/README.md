7266132d65ac62757fbfafb747ca238fb47501cf
neeraj <neerajp4321@gmail.com>
2022-09-03 12:26:05 +0530

    bar-chart-1

---
+++ bar-chart-UN-World-Population/README.md
@@ -0,0 +1,2 @@
+This is United Nation World Population Prospects Database
+data is taken from [Curran Kelleher](https://gist.githubusercontent.com/Tom-S82/1b76477754ddd9c379cfc00209e4d839/raw/65c8b7b38bbe2db900e6f1908b8df3cae048fc8e/UN_Population_Estimate_By_Country.csv) GitHub public gist
---
+++ bar-chart-UN-World-Population/index.js
@@ -0,0 +1,37 @@
+const url = 'https://gist.githubusercontent.com/Tom-S82/1b76477754ddd9c379cfc00209e4d839/raw/65c8b7b38bbe2db900e6f1908b8df3cae048fc8e/UN_Population_Estimate_By_Country.csv'
+
+d3.csv(url)
+  .then(data => {
+    const dataset = data.slice(0, 10)
+    const width = 800
+    const height = 400
+    const padding = 50
+
+    console.log(dataset[0]);
+
+    const svg = d3.select('body')
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height)
+
+    const xScale = d3.scaleLinear()
+      .domain([0, d3.max(dataset, d => +d['2020'])])
+      .range([padding, width - padding])
+
+    const yScale = d3.scaleBand()
+      .domain(dataset.map(d => d['Country']))
+      .range([padding, height])
+
+    console.log(yScale.bandwidth());
+    svg.selectAll('rect')
+      .data(dataset)
+      .enter()
+      .append('rect')
+      .attr('x', 0)
+      .attr('y', (d) => yScale(d['Country']))
+      .attr('width', (d) => xScale(+d['2020']))
+      .attr('height', yScale.bandwidth())
+
+
+
+  })
